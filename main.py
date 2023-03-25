f252458ca64583a74b075cc3891f33452447310d
Charles <charles.anyanwu36@ed.cna.nl.ca>
2023-03-25 02:50:05 -0600

    Added a simple method to print information about a card

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 
 
-
+def Display_Card(card):
+    print(card[0])
+    print(card[1])
+    print(card[2])
 def main():
     print("Blackjack Project")
+    card = ["Hearts", "Ace", 11];
+    Display_Card(card)
 
 
 
