5f211523d1fdb68f9d0762c9f60c2c678b2618f8
Charles <charles.anyanwu36@ed.cna.nl.ca>
2023-04-12 17:46:48 -0600

    changes April12

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 
 
+def display_message():
+    print("Blackjack Project")
+
+
 def Display_Card(card):
     print(card[0])
     print(card[1])
     print(card[2])
+
 def main():
-    print("Blackjack Project")
+    display_message()
     card = ["Hearts", "Ace", 11];
     Display_Card(card)
-
 
 
 if __name__ == '__main__':
